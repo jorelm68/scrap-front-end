a30103c0e51241dcd0d59e8fcedae6381b91e1ad
jorelm68 <methan@umich.edu>
2023-12-01 23:33:30 -0500

    Implemented the icons

---
+++ data/icons.js
@@ -0,0 +1,46 @@
+import { Ionicons, Feather, FontAwesome5, MaterialCommunityIcons } from '@expo/vector-icons'
+
+export const IconImage = ({ style, color, size }) => <Ionicons name="image" size={size} color={color} style={style} />
+export const IconLeaf = ({ style, color, size }) => <Ionicons name="leaf" size={size} color={color} style={style} />
+export const IconPerson = ({ style, color, size }) => <Ionicons name="person" size={size} color={color} style={style} />
+export const IconPeople = ({ style, color, size }) => <Ionicons name="people" size={size} color={color} style={style} />
+export const IconBook = ({ style, color, size }) => <Ionicons name="book" size={size} color={color} style={style} />
+export const IconPlus = ({ style, color, size }) => <Feather name="plus-circle" size={size} color={color} style={style} />
+export const IconMinus = ({ style, color, size }) => <Feather name="minus-circle" size={size} color={color} style={style} />
+export const IconCheck = ({ style, color, size }) => <Feather name="check-circle" size={size} color={color} style={style} />
+export const IconX = ({ style, color, size }) => <Feather name="x-circle" size={size} color={color} style={style} />
+export const IconUndo = ({ style, color, size }) => <Ionicons name="arrow-undo" size={size} color={color} style={style} />
+export const IconSettings = ({ style, color, size }) => <Ionicons name="settings" size={size} color={color} style={style} />
+export const IconPencil = ({ style, color, size }) => <Ionicons name="pencil" size={size} color={color} style={style} />
+export const IconEarth = ({ style, color, size }) => <Ionicons name="earth" size={size} color={color} style={style} />
+export const IconLock = ({ style, color, size }) => <Ionicons name="lock-closed" size={size} color={color} style={style} />
+export const IconFingerprint = ({ style, color, size }) => <Ionicons name="finger-print" size={size} color={color} style={style} />
+export const IconLibrary = ({ style, color, size }) => <Ionicons name="library" size={size} color={color} style={style} />
+export const IconNotification = ({ style, color, size }) => <Ionicons name="notifications" size={size} color={color} style={style} />
+export const IconHeart = ({ style, color, size }) => <Ionicons name="heart" size={size} color={color} style={style} />
+export const IconHeartOutline = ({ style, color, size }) => <Ionicons name="heart-outline" size={size} color={color} style={style} />
+export const IconHearts = ({ style, color, size }) => <FontAwesome5 name="hand-holding-heart" size={size} color={color} style={style} />
+export const IconClock = ({ style, color, size }) => <Ionicons name="time" size={size} color={color} style={style} />
+export const IconMessage = ({ style, color, size }) => <Ionicons name="chatbox-ellipses" size={size} color={color} style={style} />
+export const IconChevronDown = ({ style, color, size }) => <Ionicons name="chevron-down" size={size} color={color} style={style} />
+export const IconChevronRight = ({ style, color, size }) => <Ionicons name="chevron-forward" size={size} color={color} style={style} />
+export const IconChevronLeft = ({ style, color, size }) => <Ionicons name="chevron-back" size={size} color={color} style={style} />
+export const IconChevronUp = ({ style, color, size }) => <Ionicons name="chevron-up" size={size} color={color} style={style} />
+export const IconMenu = ({ style, color, size }) => <Ionicons name="menu" size={size} color={color} style={style} />
+export const IconEyeSlash = ({ style, color, size }) => <Ionicons name="eye-off" size={size} color={color} style={style} />
+export const IconSearch = ({ style, color, size }) => <Ionicons name="search" size={size} color={color} style={style} />
+export const IconFlash = ({ style, color, size }) => <Ionicons name="flash" size={size} color={color} style={style} />
+export const IconFlashOff = ({ style, color, size }) => <Ionicons name="flash-off" size={size} color={color} style={style} />
+export const IconRefresh = ({ style, color, size }) => <Ionicons name="refresh" size={size} color={color} style={style} />
+export const IconCircle = ({ style, color, size }) => <Ionicons name="md-ellipse-outline" size={size} color={color} style={style} />
+export const IconArrowForward = ({ style, color, size }) => <Ionicons name="arrow-forward" size={size} color={color} style={style} />
+export const IconArrowBack = ({ style, color, size }) => <Ionicons name="arrow-back" size={size} color={color} style={style} />
+export const IconZoomIn = ({ style, color, size }) => <Feather name="zoom-in" size={size} color={color} style={style} />
+export const IconZoomOut = ({ style, color, size }) => <Feather name="zoom-out" size={size} color={color} style={style} />
+export const IconHourglassFull = ({ style, color, size }) => <FontAwesome5 name="hourglass-start" size={size} color={color} style={style} />
+export const IconHourglassEmpty = ({ style, color, size }) => <FontAwesome5 name="hourglass-end" size={size} color={color} style={style} />
+export const IconTimeline = ({ style, color, size }) => <MaterialCommunityIcons name="timeline-clock" size={size} color={color} style={style} />
+
+export const defaultImage = require('../assets/icons/defaultImage.jpg')
+export const defaultCover = require('../assets/icons/defaultCover.jpg')
+export const defaultHeadshot = require('../assets/icons/defaultHeadshot.jpg')
