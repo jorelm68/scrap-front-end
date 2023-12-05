13d7c7ef020cd4b7bd27165d97f9f19f12de1d14
jorelm68 <methan@umich.edu>
2023-12-05 13:00:06 -0500

    Hid the header from the (tabs) screens

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -10,35 +10,40 @@ const Layout = () => {
                 name="feed"
                 options={{
                     tabBarLabel: 'Feed',
-                    tabBarIcon: ({ size, color }) => <Ionicons name={'home'} size={size} color={color} />
+                    tabBarIcon: ({ size, color }) => <Ionicons name={'home'} size={size} color={color} />,
+                    headerShown: false,
                 }}
             />
             <Tabs.Screen
                 name="search"
                 options={{
                     tabBarLabel: 'Search',
-                    tabBarIcon: ({ size, color }) => <Ionicons name={'search'} size={size} color={color} />
+                    tabBarIcon: ({ size, color }) => <Ionicons name={'search'} size={size} color={color} />,
+                    headerShown: false,
                 }}
             />
             <Tabs.Screen
                 name="camera"
                 options={{
                     tabBarLabel: 'Camera',
-                    tabBarIcon: ({ size, color }) => <Ionicons name={'camera'} size={size} color={color} />
+                    tabBarIcon: ({ size, color }) => <Ionicons name={'camera'} size={size} color={color} />,
+                    headerShown: false,
                 }}
             />
             <Tabs.Screen
                 name="library"
                 options={{
                     tabBarLabel: 'Library',
-                    tabBarIcon: ({ size, color }) => <Ionicons name={'library'} size={size} color={color} />
+                    tabBarIcon: ({ size, color }) => <Ionicons name={'library'} size={size} color={color} />,
+                    headerShown: false,
                 }}
             />
             <Tabs.Screen
                 name="profile"
                 options={{
                     tabBarLabel: 'Profile',
-                    tabBarIcon: ({ size, color }) => <Ionicons name={'person'} size={size} color={color} />
+                    tabBarIcon: ({ size, color }) => <Ionicons name={'person'} size={size} color={color} />,
+                    headerShown: false,
                 }}
             />
         </Tabs>
