d8e37d5cbf25707273fed09a48f4793961e44287
jorelm68 <methan@umich.edu>
2023-12-21 21:11:52 -0500

    Found the right colors

--- data/styles.js
+++ data/styles.js
@@ -3,54 +3,21 @@ export const dimensions = Dimensions.get('window')
 
 // https://paletton.com/#uid=73i050kllllaFw0g0qFqFg0w0aF
 
-export const c1 = {
-    primary0: '#AA7139',
-    primary1: '#FFD4AA',
-    primary2: '#D49F6A',
-    primary3: '#804A15',
-    primary4: '#552A00',
+export const colors = {
+    primary0: '#FFFFFF',
+    primary1: '#FFFAED',
+    primary2: '#F7E6B9',
+    primary3: '#BBA15C',
+    primary4: '#72560B',
 
-    complement0: '#AA6C39',
-    complement1: '#FFD1AA',
-    complement2: '#D49A6A',
-    complement3: '#804515',
-    complement4: '#552600',
-}
-
-export const brighterC1 = {
-    primary0: '#C2854D', // Slightly brighter than original primary0
-    primary1: '#FFE8C6', // Slightly brighter than original primary1
-    primary2: '#E6B87A', // Slightly brighter than original primary2
-    primary3: '#A0722A', // Slightly brighter than original primary3
-    primary4: '#7E471E', // Slightly brighter than original primary4
-
-    complement0: '#C27E4D', // Slightly brighter than original complement0
-    complement1: '#FFDAA6', // Slightly brighter than original complement1
-    complement2: '#D69F6A', // Slightly brighter than original complement2
-    complement3: '#99682B', // Slightly brighter than original complement3
-    complement4: '#7E471E', // Slightly brighter than original complement4 (same as primary4)
+    complement0: '#FFFFFF',
+    complement1: '#91DDFE',
+    complement2: '#46C7FF',
+    complement3: '#08B3FE',
+    complement4: '#025F87',
 };
 
-
-export const c2 = {
-    primary0: '#D3EBEB',
-    primary1: '#9ECCCC',
-    primary2: '#6AA6A6',
-    primary3: '#438989',
-    primary4: '#267070',
-
-    complement0: '#FFF1E5',
-    complement1: '#FFE0C6',
-    complement2: '#FFCCA2',
-    complement3: '#E4A46F',
-    complement4: '#BA773F',
-};
-
-
-
-
-
-export const palette = brighterC1
+export const palette = colors
 
 export const fonts = {
     itim: 'itim',
