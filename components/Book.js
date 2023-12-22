6e990043d725e6b95b353c2c50281df83b39e459
jorelm68 <methan@umich.edu>
2023-12-22 15:27:01 -0500

    Made the heart button smaller

--- components/Book.js
+++ components/Book.js
@@ -106,13 +106,12 @@ const Book = ({ book, page = 0, scraps: scrapsGiven }) => {
                 />
                 <View style={{
                     position: 'absolute',
-                    height: 200,
-                    width: 48,
+                    width: 36,
                     right: 0,
                 }}>
                     <View center style={{
-                        width: 48,
-                        height: 48,
+                        width: 36,
+                        height: 36,
                         backgroundColor: 'rgba(0, 0, 0, 0.5)',
                         borderBottomLeftRadius: 8,
                         borderTopRightRadius: 8,
@@ -125,7 +124,7 @@ const Book = ({ book, page = 0, scraps: scrapsGiven }) => {
                                 }
                             })
                         }}>
-                            <Ionicons name={user === author ? 'heart-circle' : likes.includes(user) ? 'heart' : 'heart-outline'} color={likes.includes(user) ? 'red' : palette.color6} size={36} />
+                            <Ionicons name={user === author ? 'heart-circle' : likes.includes(user) ? 'heart' : 'heart-outline'} color={likes.includes(user) ? 'red' : palette.color6} size={24} />
                         </TouchableOpacity>
                     </View>
                 </View>
