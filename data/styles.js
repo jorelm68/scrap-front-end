499371e8ba7d597cec10a22cb0daacb88f222fa8
jorelm68 <methan@umich.edu>
2023-12-22 02:26:03 -0500

    added umich colors

--- data/styles.js
+++ data/styles.js
@@ -45,9 +45,27 @@ export const greenish = {
     complement4: '#4B0082', // Even darker purple
 };
 
+export const umich = {
+    primary0: '#FFFFF0', // Super light maize
+    primary1: '#FFEB99', // Lighter maize
+    primary2: '#FFD700', // Maize
+    primary3: '#CCA300', // Darker maize
+    primary4: '#806000', // Dark dark maize
+
+    complement0: '#BFEFFF', // Super light blue
+    complement1: '#80C3FF', // Lighter blue
+    complement2: '#0080FF', // Blue
+    complement3: '#004C99', // Darker blue
+    complement4: '#00274C', // Dark dark blue
+};
 
 
-export const palette = greenish
+
+
+
+
+
+export const palette = umich
 
 export const fonts = {
     itim: 'itim',
