f565cab46e9b68118ff85803c3b6bb79c294e8e5
jorelm68 <methan@umich.edu>
2023-12-24 23:39:42 -0500

    Switched from redirect to using a direct component

--- app/(tabs)/profile/index.js
+++ app/(tabs)/profile/index.js
@@ -4,12 +4,13 @@ import { Stack, Redirect } from "expo-router";
 import { useContext } from "react";
 import { Text, View } from "react-native";
 import AppContext from '../../../context/AppContext'
+import Author from '../../../screens/Author'
 
 const Page = () => {
   const { user } = useContext(AppContext)
 
   return (
-    <Redirect href={`/profile/author/${user}`} />
+    <Author author={user} />
   )
 }
 export default Page
