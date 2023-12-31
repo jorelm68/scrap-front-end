ab4ae0287746d1fc432e1cad9bce0a7e7fb50559
jorelm68 <methan@umich.edu>
2023-12-31 03:35:59 -0500

    Fixed the list rendering of accounts in chooseACcounts

--- screens/ChooseAccount.js
+++ screens/ChooseAccount.js
@@ -32,108 +32,101 @@ const Screen = () => {
         )
     }
 
-    // This is the callback function for displaying each individual drawer in the FlatList
-    const renderItem = ({ item: account }) => {
-        return (
-            <GestureHandlerRootView>
-                <Drawer
-                    itemsTextStyle={{
-                        color: palette.color5,
-                        fontFamily: fonts.playBold,
-                    }}
-                    rightItems={[
-                        {
-                            text: 'Sign In',
-                            background: palette.color3,
-                            onPress: async () => {
-                                if (paused) return { success: false, error: 'Please don\'t click too fast' }
-                                setPaused(true)
-                                // First, make sure the author exists on the back end
-                                const response = await api.author.exists(account.author)
-                                // If there is some sort of error, forget the account
-                                if (response.error) {
-                                    setPaused(false)
-                                    Alert.alert('Error', 'Error signing into your account. Please try manually')
-                                    await utility.forgetAccount(account)
-                                    setAccounts(accounts.filter(storedAccounts => storedAccounts.author !== account.author))
-                                }
-                                else if (!response.data.exists) {
-                                    setPaused(false)
-                                    Alert.alert('Error', 'Error signing into your account. Please try manually')
-                                    await utility.forgetAccount(account)
-                                    setAccounts(accounts.filter(storedAccounts => storedAccounts.author !== account.author))
-                                }
-                                else {
-                                    // Set the account to expire one week from now
-                                    const updatedAccount = {
-                                        author: account.author,
-                                        pseudonym: account.pseudonym,
-                                        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-                                    }
-
-                                    await utility.forgetAccount(account)
-                                    await utility.saveAccount(updatedAccount)
-
-                                    await utility.storeData('autothenticate', account.author)
-                                    setUser(account.author)
-                                    const yes = await utility.hasOfflineScraps()
-                                    if (yes) {
-                                        const response1 = await utility.onlineSaveScraps(account.author)
-                                        if (response1.success) {
-                                            console.log('successfully saved scraps')
-                                        }
-                                    }
-
-                                    router.replace('/(tabs)/camera')
-                                    setPaused(false)
-                                }
-                            },
-                        },
-                        {
-                            text: 'Forget',
-                            background: palette.color4,
-                            onPress: async () => {
-                                // First, change the accounts on the device itself
-                                await utility.forgetAccount(account)
-
-                                // Then, change the accounts variable locally so that it disappears from the screen
-                                setAccounts(accounts.filter(storedAccount => storedAccount.author !== account.author))
-                            },
-                        },
-                    ]}
-                    leftItem={{
-                        text: `Expires ${utility.getDate(account.expires)}`,
-                        background: palette.color4,
-                    }}
-                >
-                    <View center padding-s4 bg-white style={{
-                        height: 60,
-                        borderBottomWidth: 1,
-                        borderBottomColor: palette.color5,
-                        backgroundColor: palette.color1,
-                    }}>
-                        <Text style={{
-                            fontFamily: fonts.jockeyOne,
-                            fontSize: 18,
-                            color: palette.color5,
-                        }}>{account.pseudonym}</Text>
-                    </View>
-                </Drawer>
-            </GestureHandlerRootView>
-        )
-    }
-
     return (
         <View style={{
             width: dimensions.width,
             height: dimensions.height,
             backgroundColor: palette.color1,
         }}>
-            <FlatList
-                data={accounts}
-                renderItem={renderItem}
-                keyExtractor={item => item.author}
-            />
+            {accounts && accounts.map((account) => {
+                return (
+                    <GestureHandlerRootView>
+                        <Drawer
+                            itemsTextStyle={{
+                                color: palette.color5,
+                                fontFamily: fonts.playBold,
+                            }}
+                            rightItems={[
+                                {
+                                    text: 'Sign In',
+                                    background: palette.color3,
+                                    onPress: async () => {
+                                        if (paused) return { success: false, error: 'Please don\'t click too fast' }
+                                        setPaused(true)
+                                        // First, make sure the author exists on the back end
+                                        const response = await api.author.exists(account.author)
+                                        // If there is some sort of error, forget the account
+                                        if (response.error) {
+                                            setPaused(false)
+                                            Alert.alert('Error', 'Error signing into your account. Please try manually')
+                                            await utility.forgetAccount(account)
+                                            setAccounts(accounts.filter(storedAccounts => storedAccounts.author !== account.author))
+                                        }
+                                        else if (!response.data.exists) {
+                                            setPaused(false)
+                                            Alert.alert('Error', 'Error signing into your account. Please try manually')
+                                            await utility.forgetAccount(account)
+                                            setAccounts(accounts.filter(storedAccounts => storedAccounts.author !== account.author))
+                                        }
+                                        else {
+                                            // Set the account to expire one week from now
+                                            const updatedAccount = {
+                                                author: account.author,
+                                                pseudonym: account.pseudonym,
+                                                expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+                                            }
+
+                                            await utility.forgetAccount(account)
+                                            await utility.saveAccount(updatedAccount)
+
+                                            await utility.storeData('autothenticate', account.author)
+                                            setUser(account.author)
+                                            const yes = await utility.hasOfflineScraps()
+                                            if (yes) {
+                                                const response1 = await utility.onlineSaveScraps(account.author)
+                                                if (response1.success) {
+                                                    console.log('successfully saved scraps')
+                                                }
+                                            }
+
+                                            router.replace('/(tabs)/camera')
+                                            setPaused(false)
+                                        }
+                                    },
+                                },
+                                {
+                                    text: 'Forget',
+                                    background: palette.color4,
+                                    onPress: async () => {
+                                        // First, change the accounts on the device itself
+                                        await utility.forgetAccount(account)
+
+                                        // Then, change the accounts variable locally so that it disappears from the screen
+                                        setAccounts(accounts.filter(storedAccount => storedAccount.author !== account.author))
+                                    },
+                                },
+                            ]}
+                            leftItem={{
+                                text: `Expires ${utility.getDate(account.expires)}`,
+                                background: palette.color4,
+                            }}
+                        >
+                            <View center padding-s4 bg-white style={{
+                                height: 60,
+                                borderBottomWidth: 1,
+                                borderBottomColor: palette.color5,
+                                backgroundColor: palette.color1,
+                            }}>
+                                <Text style={{
+                                    fontFamily: fonts.jockeyOne,
+                                    fontSize: 18,
+                                    color: palette.color5,
+                                }}>{account.pseudonym}</Text>
+                            </View>
+                        </Drawer>
+                    </GestureHandlerRootView>
+                )
+            })}
         </View>
     )
 }
