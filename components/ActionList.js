a3b0e28c93841867f6c7b01701752d9eb5817dba
jorelm68 <methan@umich.edu>
2023-12-31 03:45:32 -0500

    Fixed initial list length for actions

--- components/ActionList.js
+++ components/ActionList.js
@@ -16,7 +16,7 @@ import { defaultImage, defaultHeadshot } from '../data/icons'
 
 const Component = ({ actions, renderItem }) => {
     const { palette } = useContext(AppContext)
-    const [rows, setRows] = useState([-1, 0, 1, 2, 3, 4, 5])
+    const [rows, setRows] = useState([-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
     const scrollViewRef = useRef(null)
 
     const handleScroll = (event) => {
