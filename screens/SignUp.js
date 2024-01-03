538b446c3186297227db3461fb50451528fe0df0
jorelm68 <methan@umich.edu>
2024-01-03 17:01:57 -0500

    Added tokens when signing in and signing up

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -226,6 +226,7 @@ const Screen = () => {
                 const account = {
                   author: response.data.author,
                   pseudonym,
+                  token: response.data.token,
                   expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
                 }
 
