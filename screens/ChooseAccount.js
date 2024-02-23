a60d5d5e20689196d0943f802f883ace5300f242
jorelm68 <methan@umich.edu>
2024-02-23 13:38:40 -0500

    Made the choose account button clickable

--- screens/ChooseAccount.js
+++ screens/ChooseAccount.js
@@ -35,6 +35,54 @@ const Screen = () => {
         )
     }
 
+    const signIn = async (account) => {
+        if (paused) return { success: false, error: 'Please don\'t click too fast' }
+        setPaused(true)
+        // First, make sure the author exists on the back end and the token matches
+        const response = await api.author.autothenticate(account.author, account.token)
+        // If there is some sort of error, forget the account
+        if (response.error) {
+            setPaused(false)
+            Alert.alert('Error', 'Error signing into your account. Please try manually')
+            await utility.forgetAccount(account)
+            setAccounts(accounts.filter(storedAccounts => storedAccounts.author !== account.author))
+        }
+        else if (!response.data.autothenticate) {
+            setPaused(false)
+            Alert.alert('Error', 'Error signing into your account. Please try manually')
+            await utility.forgetAccount(account)
+            setAccounts(accounts.filter(storedAccounts => storedAccounts.author !== account.author))
+        }
+        else {
+            // Set the account to expire one week from now
+            const updatedAccount = {
+                author: account.author,
+                pseudonym: account.pseudonym,
+                token: account.token,
+                expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+            }
+
+            await utility.forgetAccount(account)
+            await utility.saveAccount(updatedAccount)
+
+            const autothenticate = { author: updatedAccount.author, token: updatedAccount.token }
+            await utility.storeData('autothenticate', JSON.stringify(autothenticate))
+            setUser(account.author)
+            const yes = await utility.hasOfflineScraps()
+            if (yes) {
+                console.log('about to save scraps')
+                const response1 = await utility.onlineSaveScraps(account.author)
+                console.log(response1)
+                if (response1.success) {
+                    console.log('successfully saved scraps')
+                }
+            }
+
+            router.replace('/(tabs)/camera')
+            setPaused(false)
+        }
+    }
+
     return (
         <ScrollView
             showsVerticalScrollIndicator={false}
@@ -58,53 +106,7 @@ const Screen = () => {
                                 {
                                     text: 'Sign In',
                                     background: palette.color3,
-                                    onPress: async () => {
-                                        if (paused) return { success: false, error: 'Please don\'t click too fast' }
-                                        setPaused(true)
-                                        // First, make sure the author exists on the back end and the token matches
-                                        const response = await api.author.autothenticate(account.author, account.token)
-                                        // If there is some sort of error, forget the account
-                                        if (response.error) {
-                                            setPaused(false)
-                                            Alert.alert('Error', 'Error signing into your account. Please try manually')
-                                            await utility.forgetAccount(account)
-                                            setAccounts(accounts.filter(storedAccounts => storedAccounts.author !== account.author))
-                                        }
-                                        else if (!response.data.autothenticate) {
-                                            setPaused(false)
-                                            Alert.alert('Error', 'Error signing into your account. Please try manually')
-                                            await utility.forgetAccount(account)
-                                            setAccounts(accounts.filter(storedAccounts => storedAccounts.author !== account.author))
-                                        }
-                                        else {
-                                            // Set the account to expire one week from now
-                                            const updatedAccount = {
-                                                author: account.author,
-                                                pseudonym: account.pseudonym,
-                                                token: account.token,
-                                                expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-                                            }
-
-                                            await utility.forgetAccount(account)
-                                            await utility.saveAccount(updatedAccount)
-
-                                            const autothenticate = { author: updatedAccount.author, token: updatedAccount.token }
-                                            await utility.storeData('autothenticate', JSON.stringify(autothenticate))
-                                            setUser(account.author)
-                                            const yes = await utility.hasOfflineScraps()
-                                            if (yes) {
-                                                console.log('about to save scraps')
-                                                const response1 = await utility.onlineSaveScraps(account.author)
-                                                console.log(response1)
-                                                if (response1.success) {
-                                                    console.log('successfully saved scraps')
-                                                }
-                                            }
-
-                                            router.replace('/(tabs)/camera')
-                                            setPaused(false)
-                                        }
-                                    },
+                                    onPress: () => signIn(account),
                                 },
                                 {
                                     text: 'Forget',
@@ -123,18 +125,20 @@ const Screen = () => {
                                 background: palette.color2,
                             }}
                         >
-                            <View center padding-s4 bg-white style={{
-                                height: 60,
-                                borderBottomWidth: 1,
-                                borderBottomColor: palette.color5,
-                                backgroundColor: palette.color1,
-                            }}>
-                                <Text style={{
-                                    fontFamily: fonts.jockeyOne,
-                                    fontSize: 18,
-                                    color: palette.color5,
-                                }}>{account.pseudonym}</Text>
-                            </View>
+                            <TouchableOpacity onPress={() => signIn(account)}>
+                                <View center padding-s4 bg-white style={{
+                                    height: 60,
+                                    borderBottomWidth: 1,
+                                    borderBottomColor: palette.color5,
+                                    backgroundColor: palette.color1,
+                                }}>
+                                    <Text style={{
+                                        fontFamily: fonts.jockeyOne,
+                                        fontSize: 18,
+                                        color: palette.color5,
+                                    }}>{account.pseudonym}</Text>
+                                </View>
+                            </TouchableOpacity>
                         </Drawer>
                     </GestureHandlerRootView>
                 )
